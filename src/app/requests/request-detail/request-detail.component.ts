1c3e6c3d3f0820f0e6781feb0bb5a4588d21e665
emilymiller4071 <emilyrauch3@gmail.com>
2023-04-29 14:12:04 -0400

    building out request-detail

--- src/app/requests/request-detail/request-detail.component.ts
+++ src/app/requests/request-detail/request-detail.component.ts
@@ -7,4 +7,30 @@ import { Component } from "@angular/core";
 })
 export class RequestDetailComponent {
     pageTitle: string = "Request Detail";
+    request: any =
+    {
+        "id": 1,
+        "description": "staff snacks",
+        "justification": "people get hungry",
+        "rejectionReason": null,
+        "deliveryMode": "Pickup",
+        "submittedDate": "2023-03-15T00:00:00",
+        "dateNeeded": "2023-04-01T00:00:00",
+        "status": "New",
+        "total": 45,
+        "userId": 1,
+        "User": {
+          "id": 1,
+          "userName": "emiller",
+          "password": "emilyIsCool",
+          "firstName": "Emily",
+          "lastName": "Miller",
+          "phone": "859-555-1234",
+          "email": "emiller@mail.com",
+          "reviewer": true,
+          "admin": true
+        }
+      }
+
+      
 }
