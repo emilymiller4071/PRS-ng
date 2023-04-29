98f8c8de4abe86e6e68943fe3405dd89d1b96841
emilymiller4071 <emilyrauch3@gmail.com>
2023-04-29 15:39:00 -0400

    building out request-detail and adjusting

--- src/app/requests/request-review/request-review.component.ts
+++ src/app/requests/request-review/request-review.component.ts
@@ -7,5 +7,75 @@ import { Component } from "@angular/core";
 })
 export class RequestReviewComponent {
     pageTitle: string = "Request Review";
-    requests: any[] = [];
+    requests: any[] = [
+        {
+            "id": 1,
+            "description": "staff snacks",
+            "justification": "people get hungry",
+            "rejectionReason": null,
+            "deliveryMode": "Pickup",
+            "submittedDate": "2023-03-15T00:00:00",
+            "dateNeeded": "2023-04-01T00:00:00",
+            "status": "Review",
+            "total": 45,
+            "userId": 1,
+            "User": {
+              "id": 1,
+              "userName": "emiller",
+              "password": "emilyIsCool",
+              "firstName": "Emily",
+              "lastName": "Miller",
+              "phone": "859-555-1234",
+              "email": "emiller@mail.com",
+              "reviewer": true,
+              "admin": true
+            }
+          },
+          {
+            "id": 3,
+            "description": "staff snacks",
+            "justification": "people get hungry",
+            "rejectionReason": null,
+            "deliveryMode": "Pickup",
+            "submittedDate": "2023-03-15T00:00:00",
+            "dateNeeded": "2023-04-06T00:00:00",
+            "status": "Review",
+            "total": 0,
+            "userId": 3,
+            "User": {
+              "id": 3,
+              "userName": "ahatfield",
+              "password": "fieldhat23",
+              "firstName": "Amy",
+              "lastName": "Hatfield",
+              "phone": "859-555-9101",
+              "email": "ahatfield@mail.com",
+              "reviewer": true,
+              "admin": false
+            }
+          },
+          {
+            "id": 5,
+            "description": "staff snacks",
+            "justification": "people get hungry",
+            "rejectionReason": null,
+            "deliveryMode": "Pickup",
+            "submittedDate": "2023-03-15T00:00:00",
+            "dateNeeded": "2023-04-12T00:00:00",
+            "status": "Review",
+            "total": 375.78,
+            "userId": 5,
+            "User": {
+              "id": 5,
+              "userName": "jmcanley",
+              "password": "JacAnley",
+              "firstName": "Jacob",
+              "lastName": "McAnley",
+              "phone": "859-555-1213",
+              "email": "jmcanley@mail.com",
+              "reviewer": true,
+              "admin": false
+            }
+          }
+    ];
 }
