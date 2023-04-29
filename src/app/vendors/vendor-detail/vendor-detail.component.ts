2ae5d7eaca91fdff244c07122faf7c3f693f24f2
emilymiller4071 <emilyrauch3@gmail.com>
2023-04-29 11:10:28 -0400

    created vendor-detail

--- src/app/vendors/vendor-detail/vendor-detail.component.ts
+++ src/app/vendors/vendor-detail/vendor-detail.component.ts
@@ -7,4 +7,18 @@ import { Component } from "@angular/core";
 })
 export class VendorDetailComponent {
     pageTitle: string = "Vendor Detail";
+    vendor: any =
+    {
+    
+        "id": 4,
+        "code": "HOB",
+        "name": "Hobby Foyer",
+        "address": "7345 Hobby Street",
+        "city": "Independence",
+        "state": "KY",
+        "zip": "41017",
+        "phone": "859-123-4567",
+        "email": "thefoyer@hobbyfoyer.com"
+          
+    }
 }
