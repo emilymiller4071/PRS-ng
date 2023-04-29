2e8342819aac8e5f1a108f613c52eacefc97208a
emilymiller4071 <emilyrauch3@gmail.com>
2023-04-29 11:02:59 -0400

    vendor-create form

--- src/app/vendors/vendor-create/vendor-create.component.ts
+++ src/app/vendors/vendor-create/vendor-create.component.ts
@@ -7,4 +7,16 @@ import { Component } from "@angular/core";
 })
 export class VendorCreateComponent {
     pageTitle: string = "Vendor Create";
+    vendor: any = 
+    {
+        "id": 2,
+        "code": "TAR",
+        "name": "Targette",
+        "address": "7171 Houston Avenue",
+        "city": "Florence",
+        "state": "KY",
+        "zip": "41020",
+        "phone": "859-345-6789",
+        "email": "bullseye@targette.com"
+    }
 }
--- src/app/vendors/vendor-list/vendor-list.component.ts
+++ src/app/vendors/vendor-list/vendor-list.component.ts
@@ -7,5 +7,50 @@ import { Component } from "@angular/core";
 })
 export class VendorListComponent {
     pageTitle: string = "Vendors List";
-    vendors: any[] = [];
+    vendors: any[] = [
+        {
+            "id": 1,
+            "code": "WAL",
+            "name": "Walburns",
+            "address": "1234 Walburns Lane",
+            "city": "Florence",
+            "state": "KY",
+            "zip": "41026",
+            "phone": "859-221-1234",
+            "email": "wallyworld@walburns.com"
+          },
+          {
+            "id": 2,
+            "code": "TAR",
+            "name": "Targette",
+            "address": "7171 Houston Avenue",
+            "city": "Florence",
+            "state": "KY",
+            "zip": "41020",
+            "phone": "859-345-6789",
+            "email": "bullseye@targette.com"
+          },
+          {
+            "id": 3,
+            "code": "MEI",
+            "name": "Meiyer",
+            "address": "555 Meiyer Drive",
+            "city": "Erlanger",
+            "state": "KY",
+            "zip": "41018",
+            "phone": "859-567-8901",
+            "email": "super@meiyer.com"
+          },
+          {
+            "id": 4,
+            "code": "HOB",
+            "name": "Hobby Foyer",
+            "address": "7345 Hobby Street",
+            "city": "Independence",
+            "state": "KY",
+            "zip": "41017",
+            "phone": "859-123-4567",
+            "email": "thefoyer@hobbyfoyer.com"
+          }
+    ];
 }
