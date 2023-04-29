54187fc8eb7926a8652b8bb638dd269dbf0c1b4f
emilymiller4071 <emilyrauch3@gmail.com>
2023-04-29 12:55:43 -0400

    product-detail component

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -7,4 +7,27 @@ import { Component } from "@angular/core";
 })
 export class ProductDetailComponent {
     pageTitle: string = "Product Detail";
+    product: any = 
+    {
+        
+        "id": 1,
+        "partNbr": "SHO",
+        "name": "uniform shoes",
+        "price": 35.97,
+        "unit": "per pair",
+        "photoPath": null,
+        "vendorId": 1,
+        "Vendor": {
+            "id": 1,
+            "code": "WAL",
+            "name": "Walburns",
+            "address": "1234 Walburns Lane",
+            "city": "Florence",
+            "state": "KY",
+            "zip": "41026",
+            "phone": "859-221-1234",
+            "email": "wallyworld@walburns.com"
+        
+        }
+    }
 }
